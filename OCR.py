0e39519b872bd21f92e7ca9a2357679c78be0bd6
J Sy <jamessy123@yahoo.com>
2022-06-30 10:57:59 -0500

    Implemented simple OCR script

--- OCR.py
+++ OCR.py
@@ -5,3 +5,11 @@ OCR Implementation using Tesseract and Open CV
 from PIL import Image
 import pytesseract
 import numpy as np
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
+
+filename = 'sample2.jpg'
+img1 = np.array(Image.open(filename))
+text = pytesseract.image_to_string(img1)
+
+print(text)

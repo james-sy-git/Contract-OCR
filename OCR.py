ab5783a6c737e5aea74c1b18590155b5a123ec9f
J Sy <jamessy123@yahoo.com>
2022-07-05 15:54:52 -0500

    Really sloppy (yet functional) version which allows concatenation of all pages of a converted PDF to be displayed.

--- OCR.py
+++ OCR.py
@@ -17,29 +17,28 @@ except ImportError as i:
 class Reader:
 
     def __init__(self, filename):
+        self.cache = ''
         self.convert()
-        self.file = 'page-0.png' # filename
+        self.files = glob.glob(r'C:\\Users\\jsy13\Desktop\\OCRScr\\' + '*.png')
+        self.process()
         try:
-            self.output = self.process()
+            print('Happy yay!')
         except OSError as e:
             print(e.errno)
 
     def process(self):
-        return pytesseract.image_to_string(self.clarify())
 
-    def clarify(self):
-        # conv = self.convert(self.file)
-        img = cv2.imread(self.file, cv2.IMREAD_GRAYSCALE)
-        # denoised = cv2.fastNlMeansDenoising(img,None,10,7,21)
-        # normalized = cv2.normalize(denoised, None, alpha=0, beta=3, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-        # gaussed = cv2.GaussianBlur(denoised, (1, 1), 0)
-        final = Image.fromarray(img)
+        for file in self.files:
+            img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+            ship = Image.fromarray(img)
+            final = ship.convert('RGB')
+            add = pytesseract.image_to_string(final)
 
-        return final.convert('RGB')
+            self.cache = self.cache + add
 
     def convert(self):
-        zoom_x = 10
-        zoom_y = 10
+        zoom_x = 7
+        zoom_y = 7
         mat = fitz.Matrix(zoom_x, zoom_y)
 
         path = r'C:\\Users\\jsy13\Desktop\\OCRScr\\'
@@ -49,10 +48,13 @@ class Reader:
             doc = fitz.open(filename)
             for page in doc:
                 pix = page.get_pixmap(matrix = mat)
-                print("made matrix")
                 pix.save(r'C:\Users\jsy13\Desktop\OCRScr\page-%i.png' % page.number)        
+
+    def pull(self, input):
+        pass
 
 # if __name__ == '__main__':
 #     test = Reader('voidedcontract.pdf')
-#     out = test.process()
+#     out = test.cache
 #     print('\n' in out)
+#     print(out)

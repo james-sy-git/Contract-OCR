716d84fc0ce203c61c02eb819af6effd36f92a2e
J Sy <jamessy123@yahoo.com>
2022-07-19 08:57:04 -0500

    Added class and method specifications

--- ContractApp.py
+++ ContractApp.py
@@ -1,6 +1,9 @@
 '''
 GUI to run OCR and Excel-loading functionality
 '''
+from plistlib import InvalidFileException
+
+
 try:
     from Loader import Loader
     from kivy.app import App
@@ -15,10 +18,31 @@ except ImportError as i:
     print(i.msg)
 
 class Root(BoxLayout):
+    '''
+    Root widget for Kivy GUI, inherits BoxLayout
+    '''
     pass
 class EverythingElse(GridLayout):
+    '''
+    Main widget for Kivy GUI, inherits GridLayout
+    
+    Class attrs:
 
-    buttondisable = False
+    att loader: this EverythingElse object's Loader
+    inv: loader is an object of class Loader
+
+    att reader: the Reader associated with this EverythingElse object
+    inv: reader is an object of class Reader
+
+    att keyword: the keyword used to search the OCR'd pdf files
+    inv: keyword must be a string (initialized as empty string)
+
+    att xlfilename: the desired filename for the saved Excel spreadsheet
+    inv: xlfilename must be a string not containing Windows' forbidden characters
+
+    att xlsheetname: the desired sheet name for the populated sheet in the Excel spreadsheet
+    inv: xlsheetname must be a string
+    '''
 
     loader = Loader()
     reader = loader.getreader()
@@ -26,32 +50,47 @@ class EverythingElse(GridLayout):
     xlfilename = None
     xlsheetname = None
 
-    def running(self):
-        return self.buttondisable
-
     def updatekey(self):
+        '''
+        Updates keyword according to TextInput field
+        '''
         self.keyword = self.ids.key.text
 
     def setkey(self):
+        '''
+        Setter for keyword
+        '''
         self.reader.setkeyword(self.keyword)
         print('keyword set:' + self.keyword)
 
     def ask_conv(self):
+        '''
+        Calls Reader object's ask_conv() method
+        '''
         self.reader.ask_conv()
         print('files set')
 
     def ask_dir(self):
+        '''
+        Calls Reader object's ask_dir() method
+        '''
         self.reader.ask_dir()
         print('directory set')
 
     def askforspreadsheet(self):
+        '''
+        Opens file dialog and sets user input as the existing Excel spreadsheet
+        '''
         try:
             input = askopenfilename()
             self.loader.setnew(input)
-        except:
-            print('something is wrong')
+        except InvalidFileException as e:
+            print(e.with_traceback())
 
     def run(self):
+        '''
+        Checks if the Reader object has its fields filled, and runs the contract parser
+        '''
         if self.reader.isready():
             self.buttondisable = True
             print('running!')
@@ -63,20 +102,35 @@ class EverythingElse(GridLayout):
             print('not ready!')
 
     def use_xl_setter(self):
+        '''
+        Uses Loader's setexcelname() method
+        '''
         if self.xlfilename != None:
             self.loader.setexcelname(self.xlfilename)
 
     def update_filename(self):
+        '''
+        Updates xlfilename according to TextInput field
+        '''
         self.xlfilename = self.ids.xlfilename.text
 
     def use_title_setter(self):
+        '''
+        Uses Loader's setwstitle() method
+        '''
         if self.xlsheetname != None:
             self.loader.setwstitle(self.xlsheetname)
 
     def update_sheetname(self):
+        '''
+        Updates xlsheetname according to TextInput field
+        '''
         self.xlsheetname = self.ids.xlsheetname.text
 
     def clear(self):
+        '''
+        Clears all populated fields
+        '''
         self.buttondisable = False
         self.keyword = ''
         self.ids.key.text = ''
@@ -86,6 +140,9 @@ class EverythingElse(GridLayout):
         self.ids.xlsheetname.text = ''
 
 class ContractApp(App):
+    '''
+    Class to house and run GUI, extends App
+    '''
     
     def build(self):
         root = Root()
